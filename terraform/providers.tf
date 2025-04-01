122f37bbddf7554fab51701f66fccf95743a69ba
théau Yapi <theau.yapi@ynov.com>
2025-04-01 12:28:12 +0200

    cd push

---
+++ terraform/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">=1.3.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.47.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">=2.9.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "a90938fc-21d0-4f36-80a8-c4c901d3d2d1"
+}
+
